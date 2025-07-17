cd399dbb9ac8fbad1798c5d37337ef893ee9e57c
daey <kodhyoung__@naver.com>
2025-07-17 20:58:08 +0900

    init commit

---
+++ src/main/java/daedaedae/nihongo_news_backend/NihongoNewsBackendApplication.java
@@ -0,0 +1,13 @@
+package daedaedae.nihongo_news_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NihongoNewsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NihongoNewsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/daedaedae/nihongo_news_backend/NihongoNewsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package daedaedae.nihongo_news_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NihongoNewsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
